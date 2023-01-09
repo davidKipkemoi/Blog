d010ed94f61f810d3ee663fadfcb2d832f7bade8
Kiprich <davidkipkemoi30@gmail.com>
2023-01-09 20:07:23 +0300

    Header

--- src/Components/topbar/TopBar.jsx
+++ src/Components/topbar/TopBar.jsx
@@ -4,10 +4,28 @@ export default function TopBar() {
     <>
    <div className="top">
       <div className="topLeft">
-        Left
+        <i className="topIcon fa-brands fa-twitter"> </i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
    </div>
-      <div className="topCenter">Center</div>
-      <div className="topRight">Right</div>
+
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Write</li>
+          <li className="topListItem">Logout</li>
+        </ul>
+        </div>
+
+      <div className="topRight">
+        <img
+          className="topImg"
+          src="https://images.pexels.com/photos/6056462/pexels-photo-6056462.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <i className="topSearch fa-solid fa-magnifying-glass"></i>
+        </div>
    </div>
     
     </>
