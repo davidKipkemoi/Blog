ffbe842f490b911905d9f907fffa1b7a3bd46800
Kiprich <davidkipkemoi30@gmail.com>
2023-01-23 23:34:05 +0300

    M.signup

---
+++ api/routes/auth.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+/* Registration*/
+router.post('/signup', async(req,res)=>{
+    try {
+        const newUser = new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password
+        });
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(err)
+    }
+})
