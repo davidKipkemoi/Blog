224bcad83e66d875b01f9c8dd917e789b0247133
Kiprich <davidkipkemoi30@gmail.com>
2023-01-23 22:19:20 +0300

    api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+
+dotenv.config();
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL).then(console.log('Mongoose Connected')).catch(err=>console.log(err))
 
 
 
