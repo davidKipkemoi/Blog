22e4712f43ba05bb6480f15d34a7cb6722054149
Kiprich <davidkipkemoi30@gmail.com>
2023-01-25 22:52:41 +0300

    Register api

--- api/index.js
+++ api/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const authRoute = require('./routes/auth')
+const authRoute = require('./routes/auth');
+const router = require("./routes/auth")
 dotenv.config();
-
 
 mongoose.connect(
     process.env.MONGO_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify:true },
     () => {
       console.log("Connected to MongoDB");
     }
@@ -18,7 +22,7 @@ mongoose.connect(
 
 
 app.use(express.json())
-app.use("/api/auth ", authRoute);
+app.use("/api/auth",authRoute);
 app.listen("9000", ()=>{
-    console.log('Server started')
+    console.log('Server started');
 });
