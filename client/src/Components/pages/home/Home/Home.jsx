84a8cdd2ad2093774af91ffeb55e159ba84115ec
Kiprich <davidkipkemoi30@gmail.com>
2023-02-01 16:35:42 +0300

    Client-Side

--- client/src/Components/pages/home/Home/Home.jsx
+++ client/src/Components/pages/home/Home/Home.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import Header from '../../../header/Header'
 import Posts from '../../../posts/Posts'
 import Sidebar from '../../../sidebar/Sidebar'
 import "./home.css"
+import axios from 'axios'
 
 export default function Home() {
+  const [posts,setPosts] = useState([]);
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      const res = await axios.get("/posts");
+      setPosts(res.data)
+
+    }
+   fetchPosts();
+  },[])
   return (
     <>
     <Header/>
       <div className='home'>
-        <Posts/>
+        <Posts fPost ={posts}/>
         <Sidebar/>
     </div>
     </>
--- client/src/Components/posts/Posts.jsx
+++ client/src/Components/posts/Posts.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Post from '../post/Post'
 import "./posts.css"
 
-export default function Posts() {
+export default function Posts({fPost}) {
   return (
     <div className='posts'>
-    <Post img="https://images.pexels.com/photos/5125528/pexels-photo-5125528.jpeg" />
-      <Post img="https://images.pexels.com/photos/1233286/pexels-photo-1233286.jpeg" />
-      <Post img="https://images.pexels.com/photos/6711867/pexels-photo-6711867.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/5490778/pexels-photo-5490778.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
-      <Post img="https://images.pexels.com/photos/4916559/pexels-photo-4916559.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"/>
+   {fPost.map((p)=>(
+     <Post  img="https://images.pexels.com/photos/5125528/pexels-photo-5125528.jpeg"/>
+   ))}
+   
+   
     </div>
   )
 }
