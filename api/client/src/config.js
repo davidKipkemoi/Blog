857f71467397355ddc040b1634d2139ba000cc0c
Kiprich <davidkipkemoi30@gmail.com>
2023-05-13 22:37:33 +0300

    changes

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,4 +1,4 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-   baseURL: "https://safari-blog.herokuapp.com/api"
+   baseURL: "https://safari-app.herokuapp.com/"
    });
--- api/index.js
+++ api/index.js
@@ -8,12 +8,14 @@ const categoryRoute = require('./routes/categories');
 const postsRoute = require('./routes/posts');
 const multer = require('multer')
 const path = require("path");
-const cors = require('cors')
+const cors = require('cors');
+const PORT = process.env.PORT || 8080
+const bodyParser = require('body-parser')
 
 dotenv.config();
 
 mongoose.connect(
-    process.env.MONGO_URL,
+    process.env.MONGO_URI,
     { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -42,17 +44,26 @@ const upload = multer({storage:storage});
 
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/categories",categoryRoute);
 app.use("/api/posts",postsRoute);
 app.use("/images", express.static(path.join(__dirname,"/images")))
 
-app.use(express.static(path.join(__dirname,"/client/build")));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,"/client/build",'index.html'));
-});
 
-app.listen(process.env.PORT || 9000, ()=>{
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static("/client/build"));
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+}
+
+
+
+
+app.listen(PORT, ()=>{
     console.log('Server started');
 });
