6a81aa9505020d9ea4ddfcf52a007c03a1b7e5ad
Kiprich <davidkipkemoi30@gmail.com>
2023-05-16 20:42:57 +0300

    my first commit

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,4 +1,4 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-   baseURL: "https://safari-app.herokuapp.com/api/",
+   baseURL: "https://safari-blog.herokuapp.com/api/",
    });
