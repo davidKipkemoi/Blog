cc859664586981bda8d63306e67473e607ad501b
Kiprich <davidkipkemoi30@gmail.com>
2023-06-10 19:10:41 +0300

    change images

--- client/src/Components/post/Post.jsx
+++ client/src/Components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import {Link} from 'react-router-dom'
 
 export default function Post({post}) {
-  const PF = "https://safarierblog.netlify.app/api/images/"
+  const PF = "https://safari-blog.onrender.com/images/"
   return (
     <div className="post">
       {post.photo && <img
