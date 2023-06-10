2a3c8e0a1df16ceb7ac838eb455016fc7951a924
Kiprich <davidkipkemoi30@gmail.com>
2023-06-10 18:36:53 +0300

    change

--- client/src/Components/post/Post.jsx
+++ client/src/Components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import {Link} from 'react-router-dom'
 
 export default function Post({post}) {
-  const PF = "https://safari-blog.onrender.com/api/images/"
+  const PF = "/images/"
   return (
     <div className="post">
       {post.photo && <img
